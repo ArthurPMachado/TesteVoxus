8e364a5482569a105f4e2c3ef0971ee2ed08a645
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-05-28 18:03:58 -0300

    Finish PaymentBO with validations

--- SistemaDePagamentos/src/br/com/voxus/bo/PaymentBO.java
+++ SistemaDePagamentos/src/br/com/voxus/bo/PaymentBO.java
@@ -1,5 +1,10 @@
 package br.com.voxus.bo;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 import br.com.voxus.dao.PaymentDAO;
 import br.com.voxus.to.PaymentTO;
 
@@ -12,8 +17,32 @@ public class PaymentBO {
 	}
 	
 	public boolean cadastrarPagamento(PaymentTO payment) {
-		if(payment.getTitle() < 5)
+		// Valida o tamanho do titulo do pagamento
+		if(payment.getTitle().length() < 5 || payment.getTitle().length() > 100) {
+			JOptionPane.showMessageDialog(null, "Tamanho inválido para o título, tente colocar entre 5 e 100 caracteres");
+			return false;
+		}
+		
+		// Formatar a data para  ano/mes/dia
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		sdf.format(payment.getDate());
+		
+		// A taxa é um valor fixo de 5% do valor do pagamento
+		double tax = payment.getValue() * 0.05;
+		payment.setTax(tax);
 		
 		return dao.create(payment);
+	}
+	
+	public List<PaymentTO> listarPagamentos() {
+		return dao.readPayments();
+	}
+	
+	public boolean atualizarPagamentos(PaymentTO payment) {
+		return dao.update(payment);
+	}
+	
+	public void deletarPagamentos(int código) {
+		dao.delete(código);
 	}
 }
