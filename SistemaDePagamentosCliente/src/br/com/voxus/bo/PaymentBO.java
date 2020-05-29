2a0bfa6d2baac8c9b88ea88d586c269d7ed26cae
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-05-28 21:44:08 -0300

    Start PaymentBO

---
+++ SistemaDePagamentosCliente/src/br/com/voxus/bo/PaymentBO.java
@@ -0,0 +1,12 @@
+package br.com.voxus.bo;
+
+import br.com.voxus.servico.Servico;
+
+public class PaymentBO {
+
+	private Servico servico = null;
+	
+	public PaymentBO() {
+		servico = new Servico();
+	}
+}
