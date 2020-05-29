7ca34c1e6aa3d7e4cd6327648321414ad1b3507a
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-05-28 21:53:58 -0300

    Finish PaymentBO

--- SistemaDePagamentosCliente/src/br/com/voxus/bo/PaymentBO.java
+++ SistemaDePagamentosCliente/src/br/com/voxus/bo/PaymentBO.java
@@ -1,6 +1,7 @@
 package br.com.voxus.bo;
 
 import br.com.voxus.servico.Servico;
+import br.com.voxus.to.PaymentTO;
 
 public class PaymentBO {
 
@@ -8,5 +9,21 @@ public class PaymentBO {
 	
 	public PaymentBO() {
 		servico = new Servico();
+	}
+	
+	public void cadastrar(PaymentTO payment) {
+		servico.servicePost(payment);
+	}
+
+	public PaymentTO[] listar() {
+		return servico.serviceGetAll();
+	}
+	
+	public void atualizar(PaymentTO payment) {
+		servico.servicePut(payment, payment.getId());
+	}
+	
+	public void excluir(int codigo) {
+		servico.serviceDelete(codigo);
 	}
 }
