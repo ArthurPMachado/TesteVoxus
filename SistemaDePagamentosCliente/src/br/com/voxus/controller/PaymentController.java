a4a31db884591c0dbde943ca931218ca5896884f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-05-28 22:37:55 -0300

    Finish controller with CRUD

--- SistemaDePagamentosCliente/src/br/com/voxus/controller/PaymentController.java
+++ SistemaDePagamentosCliente/src/br/com/voxus/controller/PaymentController.java
@@ -1,11 +1,15 @@
 package br.com.voxus.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.voxus.bo.PaymentBO;
+import br.com.voxus.to.PaymentTO;
 
 /**
  * Servlet implementation class PaymentController
@@ -14,20 +18,79 @@ import javax.servlet.http.HttpServletResponse;
 public class PaymentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
+	private PaymentBO bo = new PaymentBO();
+	private PaymentTO payment = null;
 	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public PaymentController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		if(request.getRequestURI().equals("/SistemaDePagamentosCliente/listar")) {
+			
+			listar(request, response);
+			
+		} else if(request.getRequestURI().equals("/SistemaDePagamentosCliente/adicionar")) {
+			
+			adicionar(request, response);
+			
+		} else if(request.getRequestURI().equals("/SistemaDePagamentosCliente/alterar")) {
+			
+			alterar(request, response);
+		
+		} else if(request.getRequestURI().equals("/SistemaDePagamentosCliente/excluir")) {
+			
+			remover(request, response);
+			
+		}
+	}
+	
+	
+	protected void adicionar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		payment = new PaymentTO();
+		payment.setTitle(request.getParameter("title"));
+		payment.setValue(Double.parseDouble(request.getParameter("value")));
+		payment.setDate(request.getParameter("date"));
+		payment.setComments(request.getParameter("comments"));
+		
+		bo.cadastrar(payment);
+		
+		request.setAttribute("controle", "lista-produto");
+		
+		request.getRequestDispatcher("listar").forward(request, response);
 	}
 
+	protected void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PaymentTO[] paymentArray = bo.listar();
+		
+		request.setAttribute("listaDePagamentos", paymentArray);
+		
+		request.setAttribute("controle", "lista-produto");
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+	
+	protected void alterar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		payment.setId(Integer.parseInt(request.getParameter("paymentIdPut")));
+		payment.setTitle(request.getParameter("title"));
+		payment.setValue(Double.parseDouble(request.getParameter("value")));
+		payment.setDate(request.getParameter("date"));
+		payment.setComments(request.getParameter("comments"));
+		
+		bo.atualizar(payment);
+		
+		request.setAttribute("controle", "lista-produto");
+		
+		request.getRequestDispatcher("listar").forward(request, response);
+	}
+	
+	protected void remover(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int codigo = Integer.parseInt(request.getParameter("paymentIdDelete"));
+		
+		bo.excluir(codigo);
+		
+		request.setAttribute("controle", "lista-produto");
+		
+		request.getRequestDispatcher("listar").forward(request, response);
+	}
 }
